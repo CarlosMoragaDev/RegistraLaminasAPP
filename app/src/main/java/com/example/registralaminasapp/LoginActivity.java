616f4b23b49c5cdd24150f42c543f02b16531a4c
Carlos Moraga <c.moraga.tradu@gmail.com>
2022-10-18 23:43:43 -0300

    No se permite campos vacios en login

--- app/src/main/java/com/example/registralaminasapp/LoginActivity.java
+++ app/src/main/java/com/example/registralaminasapp/LoginActivity.java
@@ -4,24 +4,61 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText emailUser;
+    private EditText passUser;
+    private Button btnInicio;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+
+        emailUser = (EditText) findViewById(R.id.emailUser);
+        passUser = (EditText) findViewById(R.id.passUser);
+        btnInicio = (Button) findViewById(R.id.btnInicio);
+
+        btnInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!emailUser.getText().toString().isEmpty() && !passUser.getText().toString().isEmpty()){
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.putExtra("nombre", emailUser.getText().toString());
+                    startActivity(intent);
+
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, "Debes llenar todos los campos", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+
+    public void nextPage(View vista){
+        Intent nextPage = new Intent(this, RegistroActivity.class);
+        startActivity(nextPage);
     }
 
     public void nextPageRes(View vista){
-        Toast toast = Toast.makeText(this, "Trabajando", Toast.LENGTH_SHORT);
+        Toast toast = Toast.makeText(this, "Pronto disponible", Toast.LENGTH_SHORT);
         toast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.START, 90,0);
         toast.show();
     }
 
+
     public void nextPage2(View vista1){
+
+
         String mensaje = "Bienvenido";
         Toast toast = Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT);
         toast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.START, 90,0);
@@ -30,11 +67,6 @@ public class LoginActivity extends AppCompatActivity {
         Intent nextPage = new Intent(this, MainActivity.class);
         startActivity(nextPage);
 
-    }
-
-    public void nextPage(View vista){
-        Intent nextPage = new Intent(this, RegistroActivity.class);
-        startActivity(nextPage);
     }
 
 }
