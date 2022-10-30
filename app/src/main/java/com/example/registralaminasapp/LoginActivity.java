9a893487ce984f764d75f36d90e97018e4e3e37e
Carlos Moraga <c.moraga.tradu@gmail.com>
2022-10-30 00:34:37 -0300

    -Se cambió el layout del login para ser todo en estilo Relative -Se habilitó el ingreso por huella al inicio

--- app/src/main/java/com/example/registralaminasapp/LoginActivity.java
+++ app/src/main/java/com/example/registralaminasapp/LoginActivity.java
@@ -8,12 +8,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
+
+import java.util.concurrent.Executor;
 
 public class LoginActivity extends AppCompatActivity {
+
     private EditText emailUser;
     private EditText passUser;
     private Button btnInicio;
+    private Button btnHuella;
 
 
     @Override
@@ -25,6 +32,7 @@ public class LoginActivity extends AppCompatActivity {
         emailUser = (EditText) findViewById(R.id.emailUser);
         passUser = (EditText) findViewById(R.id.passUser);
         btnInicio = (Button) findViewById(R.id.btnInicio);
+        btnHuella = (Button) findViewById(R.id.btnHuella);
 
         btnInicio.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,25 +48,36 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+
+        btnHuella.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                        .setTitle("Porfavor utilice su huella")
+                        .setDescription("Autenticación de Usuario")
+                        .setNegativeButtonText("Cancelar")
+                        .build();
+                getPrompt().authenticate(promptInfo);
+
+            }
+        });
     }
 
-
-
+    //para ir a registro usuario
     public void nextPage(View vista){
         Intent nextPage = new Intent(this, RegistroActivity.class);
         startActivity(nextPage);
     }
 
+    //para ir a recuperar contrseña
     public void nextPageRes(View vista){
         Toast toast = Toast.makeText(this, "Pronto disponible", Toast.LENGTH_SHORT);
         toast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.START, 90,0);
         toast.show();
     }
 
-
+    //para ir a home agregando nombre de usuario
     public void nextPage2(View vista1){
-
-
         String mensaje = "Bienvenido";
         Toast toast = Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT);
         toast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.START, 90,0);
@@ -66,7 +85,36 @@ public class LoginActivity extends AppCompatActivity {
 
         Intent nextPage = new Intent(this, MainActivity.class);
         startActivity(nextPage);
-
     }
+
+    //recursos para ingreso biometrico
+    private BiometricPrompt getPrompt()
+    {
+        Executor executor = ContextCompat.getMainExecutor(this);
+        BiometricPrompt.AuthenticationCallback callback = new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                Toast.makeText(LoginActivity.this,errString, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                intent.putExtra("nombre", emailUser.getText().toString());
+                startActivity(intent);
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                Toast.makeText(LoginActivity.this, "Error al leer huella", Toast.LENGTH_SHORT).show();
+            }
+        };
+        BiometricPrompt biometricPrompt = new BiometricPrompt(this,executor,callback);
+        return biometricPrompt;
+    }
+
 
 }
