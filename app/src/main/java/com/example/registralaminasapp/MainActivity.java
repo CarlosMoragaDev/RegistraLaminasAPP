1b19d854e049cf086ac0f774fb6c523aa5dc9b12
Carlos Moraga <c.moraga.tradu@gmail.com>
2022-10-30 00:50:49 -0300

    -Se agrego un proceso al activity main con AsyncTask para crear un Album

--- app/src/main/java/com/example/registralaminasapp/MainActivity.java
+++ app/src/main/java/com/example/registralaminasapp/MainActivity.java
@@ -3,14 +3,22 @@ package com.example.registralaminasapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Bundle bundle;
     private TextView saludoInicio;
+
+    //clasesasynctask
+    Button btnAlbumProceso;
+    ProgressBar progressBarProceso;
 
 
     @Override
@@ -26,6 +34,12 @@ public class MainActivity extends AppCompatActivity{
 
         saludoInicio.append(" " + saludo);
 
+        //AsyncTask
+        btnAlbumProceso = (Button) findViewById(R.id.btnAlbumProceso);
+        progressBarProceso = (ProgressBar) findViewById(R.id.progressBarProceso);
+
+        btnAlbumProceso.setOnClickListener(this);
+
 
     }
     public void nextPageAlbum(View vista){
@@ -40,6 +54,69 @@ public class MainActivity extends AppCompatActivity{
         startActivity(nextPage);
 
     }
+
+    //componentes de proceso AsyncTask
+    //ASyncTask
+
+    private void UnSegundo(){
+        try{
+            Thread.sleep(1000);
+        }catch (InterruptedException e){}
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnAlbumProceso:
+                EjemploAsyncTask ejemploAsyncTask = new
+                        EjemploAsyncTask();
+                ejemploAsyncTask.execute();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private class EjemploAsyncTask extends
+            AsyncTask<Void,Integer,Boolean> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBarProceso.setMax(100);
+            progressBarProceso.setProgress(0);
+        }
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            for(int i=1; i<=10; i++){
+                UnSegundo();
+                publishProgress(i*10);
+                if(isCancelled()){
+                    break;
+                }
+            }
+            return true;
+        }
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            progressBarProceso.setProgress(values[0].intValue());
+        }
+        @Override
+        protected void onPostExecute(Boolean resultado) {
+            //super.onPostExecute(aVoid);
+            if(resultado){
+                Toast.makeText(getBaseContext(),
+                        "Creacion de Album Finalizada en AsyncTask", Toast.LENGTH_LONG).show();
+            }
+        }
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+            Toast.makeText(getBaseContext(),
+                    "La creacion ha sido cancelada", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
 
 
 }
